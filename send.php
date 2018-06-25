2dbd52c3808480de4448e1d6bbd7e0bddb119453
Tyler <divinetsolutions@gmail.com>
2018-06-25 10:26:12 -0400

    first commit

---
+++ send.php
@@ -0,0 +1,29 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $email_from = 'ashleyfreeze5823@gmail.com';
+
+    $email_subject = "Contact Form";
+
+    $email_body = "$name.\n".
+        "$email.\n".
+        "$message.\n";
+
+    $to = "ashleyfreeze5823@gmail.com";
+
+    $headers = "From: $email \r\n";
+
+    $header .= "Reply-To: $email \r\n";
+
+    mail($to, $email_subject, $email_body, $headers);
+
+    header("Location: contact");
+
+    $submit = $_POST['submit'];
+
+    if(isset($submit)) {
+        echo "Successful!";
+    }
+?>
